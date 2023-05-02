581e2d9dc3ffb97a3e96cd3db2bd0bc505423228
MyeongwonKim <mw7539@naver.com>
2023-05-02 11:56:17 +0900

    todo별 등록 시간 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -31,9 +31,9 @@ const TodoList = () => {
 
   const getTodos = async () => {
     // Firestore 쿼리를 만듭니다.
-    const q = query(todoCollection);
+    // const q = query(todoCollection);
     // const q = query(collection(db, "todos"), where("user", "==", user.uid));
-    // const q = query(todoCollection, orderBy("datetime", "desc"));
+    const q = query(todoCollection, orderBy("datetime", "desc"));
 
     // Firestore 에서 할 일 목록을 조회합니다.
     const results = await getDocs(q);
@@ -64,14 +64,21 @@ const TodoList = () => {
     // }
     // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
 
+    const date = new Date().toISOString().split("T")[0];
+    const time = new Date().toTimeString().split(" ")[0];
+    const dateTime = date + ' ' + time
+
     // Firestore 에 추가한 할 일을 저장합니다.
     const docRef = await addDoc(todoCollection, {
       text: input,
       completed: false,
+      datetime: dateTime
     });
 
     // id 값을 Firestore 에 저장한 값으로 지정합니다.
-    setTodos([...todos, { id: docRef.id, text: input, completed: false }]);
+    setTodos([...todos, { id: docRef.id, text: input, completed: false, datetime: dateTime }]);
+    // let res = todos.sort((a, b) => a.dateTime - b.dateTime)
+    // setTodos(res)
     setInput("");
   };
 
