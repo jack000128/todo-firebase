79ef6a5cc5166a25d30a3261e39e2d0f0231bb72
MyeongwonKim <mw7539@naver.com>
2023-05-09 11:48:03 +0900

    카카오 완료

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,16 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from "react";
 import TodoList from "../components/TodoList";
 
 export default function Home() {
+  const router = useRouter()
+  const { data } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.replace("/auth/signin")
+    }
+  })
   return (
     <div>
       <TodoList />
